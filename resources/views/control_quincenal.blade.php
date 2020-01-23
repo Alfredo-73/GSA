f78c3bceeb6fa81be57cac645b5f17a59115497e
Anto <atorresgolf@gmail.com>
2020-01-23 19:05:10 -0300

    ultimos cambios

--- resources/views/control_quincenal.blade.php
+++ resources/views/control_quincenal.blade.php
@@ -8,7 +8,7 @@
                 <div class="px-4">
                     <div class="table-wrapper">
                         <h1 class="text-center">CONTROL QUINCENAL DE FACTURACION Y PAGO</h1>
-                         <a id="agregar" class="btn btn-success mb-5 rounded" href="{{ url('/nuevo_control') }}" role="button" style="margin-left:72rem" >NUEVO </a>
+                         <a id="agregar" class="btn primary-color-dark mb-5 rounded"  href="{{ url('/nuevo_control') }}" role="button" style="margin-left:72rem;color:white" >NUEVO </a>
 
                         <!--BOTON AGREGAR PRODUCTO-------------------
                         
@@ -17,7 +17,7 @@
                         </form> -->
                     </div>
                     <!--Table-->
-                    <table class="table-responsive-sm table-striped w-auto mx-auto">
+                    <table class="table-striped w-auto mx-auto">
 
                         <!--Table head-->
                         <thead>
@@ -107,12 +107,11 @@
                                 <td class="text-center" hidden="true">$ {{$disponible - $totalPago}}</td>
                                 <td class="text-center">$ {{($disponible - $totalPago)/$control->toneladas}}</td>
 
-                                <td class="text-center"></td>
-                                <td>
+                                  <td>
                                        <form method="POST" action="{{ url('/borrar_control/'.$control->id) }}">
                                             {{ csrf_field() }}
                                             {{ method_field('DELETE') }}
-                                            <button type="submit" onclick="return confirm('¿Desea eliminar el control quincenal?')" id= "borrar" class="btn btn-danger btn-rounded mb-4"> BORRAR
+                                            <button type="submit" onclick="return confirm('¿Desea eliminar el control quincenal?')" id= "borrar" class="btn peach-gradient mb-1 btn-sm m-0 text-center"> BORRAR
                                             </button>
 
                                            <!--<button class="btn btn-danger" type="submit" id="borrar">Borrar</button>-->
@@ -122,30 +121,27 @@
                                         <button class="btn btn-danger btn-rounded mb-4" type="submit" id="borrar">Borrar</button>
                                     </form> -->
                                 </td>
-                                <!--BOTON MODIFICAR NO FUNCIONA LA VISTA MODIFPRODUCTO, SI TOMA EL ID DEL PREODUCTO-------->
+                                <!--BOTON MODIFICAR NO FUNCIONA LA VISTA MODIFPRODUCTO, SI TOMA EL ID DEL PREODUCTO------
                                 <td>
-                                     <a id="modificar" class="btn btn-primary btn-rounded mb-4" href="/modif_control/{{$control->id}}" role="button" >Modificar </a>
+                                     <a id="modificar" class="btn btn-primary btn-rounded mb-4" href="/modif_control/{{$control->id}}" role="button" >Modificar </a>-->
 
                                   <!-- <form method="POST" action="">
                                         <button class="btn btn-primary btn-rounded mb-4" type="submit" id="borrar">Modifica</button>
-                                    </form> -->
-                                </td>
+                                    </form>
+                                </td> -->
+                                
                                 <td>
-                                    <a href="/modal_control/{{ $control->id }}" class="btn btn-default btn-rounded mb-4" data-toggle="modal" data-target="#modal_control{{ $control->id }}">Ver/Imp.
+                                    <form method="PUT" action="/modal_control/{{ $control->id }}">
+                                        @csrf
+                                        {{method_field('PUT')}}
+                                        <p href="/modal_control/{{ $control->id }}" class="btn blue-gradient mb-1 btn-sm m-0 text-center" data-toggle="modal" data-target="#modal_control{{ $control->id }}" form method="POST" action="/modal_control/{{$control->id}}">VER</p>
+                                        @csrf
+                                        {{method_field('PUT')}}   
                                         @include('modal_control')
-                                    </a>
+                                    </form>
                                 </td>
-                                <td>
-                                    <a href="{{action('controlController@downloadPDF', $control->id)}}" class="far fa-file-pdf fa-3x"   >
-                                        
-                                    </a>
-                                </td>
-                                 <td>
-                                    <a href="{{action('controlController@verPDF', $control->id)}}" class="btn btn-default btn-rounded mb-4 btn-success"  >Ver PDF
-
-                                    </td>
-
-                                    
+                                
+                                          
                             </tr>
                         @endforeach
                         
