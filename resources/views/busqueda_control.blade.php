e9767d81c5f2492ae8d6d7f5563d317480986621
Anto <atorresgolf@gmail.com>
2020-01-28 21:45:25 -0300

    scope

--- resources/views/busqueda_control.blade.php
+++ resources/views/busqueda_control.blade.php
@@ -1,6 +1,80 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="panel panel-success">
+		<div class="panel-heading">Buscar</div>
+		<form action="/busqueda_control" method="get" onsubmit="return showLoad()">
+		<div class="panel-body">
+			<label class="label-control">Buscar</label>
+			<input type="text" name="control_id" class="form-control" placeholder="Please input stock name/description" required="required">
+			<br>
+
+	</div>
+	<div class="panel-footer">
+		<button type="submit" class="btn btn-success">Buscar</button>
+	</div>
+	</form>
+</div>
+@if (isset($cliente))
+			<div class="panel panel-success">
+				<div class="panel-heading">Resultado de busqueda</div>
+				<div class="panel-body">
+
+					<div class='table-responsive'>
+					  <table class='table table-bordered table-hover'>
+					    <thead>
+					      <tr>
+					        <th>ID</th>
+					        <th>Nombre</th>
+					        <th>CUIT</th>
+
+										<th>nombre_nota</th>
+					      </tr>
+					    </thead>
+					    <tbody>
+
+<!--foreach($noticias as $noti){
+    echo $noti->noticiero_programa;
+    foreach($noti->notas as $notas){
+        echo $noti->nombre_nota;
+    }
+}-->
+
+					@foreach($cliente as $buscar)
+					<tr>
+						<td>{{$buscar['quincena-id']}}</td>
+						<td>{{$buscar['factura']}}</td>
+						<td>{{$buscar['importe']}}</td>
+
+                    </tr>
+
+
+
+
+
+
+       @endforeach
+
+				</tbody>
+
+
+
+
+			</table>
+{{ $cliente->appends(Request::only('contrrol_id'))->links() }}
+		</div>
+	</div>
+		<div class="panel-footer">
+		<a href="{{url('noticia/buscar')}}" class="btn btn-warning">Reiniciar busqueda</a>
+		</div>
+</div>
+@endif
+
+@stop
+
+
+
+<!--
 <div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-ms-12 col-md-12 col-lg-12">
@@ -10,37 +84,26 @@
                         <h1 class="text-center">CONTROL QUINCENAL DE FACTURACION Y PAGO</h1>
                         <a id="agregar" class="btn primary-color-dark mb-5 rounded" href="{{ url('/nuevo_control') }}" role="button" style="margin-left:72rem;color:white">NUEVO </a>
 
-                        <!--BOTON AGREGAR PRODUCTO-------------------
-                        
-                        <form method="POST" action="">
-                            <button class="btn btn-success mb-5" type="submit" id="agregar" style="margin-left:75rem">NUEVO</button>
-                        </form> -->
                     </div>
 
-                  
-                        <form class="form-inline md-form mr-auto mb-4 float-right" action="">
-                        <select name="buscarporcliente" class="form-control mr-sm-2" id="exampleFormControlSelect1">
-                        <option>Cliente</option>
-                                @foreach($clientes as $cliente)
-                        <option value="{{$cliente->id}}">{{$cliente->nombre}}</option>
-                        @endforeach
-                    
-                        </select>
-                        <select name="buscarporquincena" class="form-control mr-sm-2" id="exampleFormControlSelect1">
-                        <option>Quincena</option>
-                        @foreach($quincenas as $quincena)
-                        <option value="{{$quincena->id}}">{{$quincena->nombre}}</option>
-                        @endforeach
-                        </select>
 
-                        <button class="btn aqua-gradient btn-rounded btn-md" type="submit">Buscar</button>
-                        <a class="fas fa-sync-alt" role="button" href=  {{ url('/control_quincenal') }}  style="cursor:pointer" title="refrescar" name="Refrescar" >  Refrescar</a>
+ <nav class="navbar navbar-expand-lg navbar-dark indigo mb-4">
 
-                        </form>
+  <a class="navbar-brand" href="#">Buscador</a>
 
-                   
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-<!--<nav class="navbar navbar-light float-right">
+    <form class="form-inline ml-auto">
+      <div class="md-form my-0">
+        <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+      </div>
+      <button href="#!" class="btn btn-outline-white btn-md my-0 ml-sm-2" type="submit">Buscar</button>
+    </form>
+
+  </div>
+
+</nav>
+<nav class="navbar navbar-light float-right">
     <form class="form-inline md-form mr-auto mb-4" action="">
  <select name="tipo" class="form-control mr-sm-2" id="exampleFormControlSelect1">
       <option>Buscar por ...</option>
@@ -54,92 +117,65 @@
   <button class="btn aqua-gradient btn-rounded btn-sm my-0" type="submit">Buscar</button>
 
     </form>
-   <a class="fas fa-undo" role="button" href=  {{ url('/control_quincenal') }} style='margin-left:5rem' style="cursor:pointer",name="Regresar" >  Refrescar</a>
+   <a class="fas fa-undo" role="button" href=  {{ url('/abm_cliente') }} style='margin-left:5rem' style="cursor:pointer",name="Regresar" >  Refrescar</a>
 
-</nav>-->
-<!-- <form class="form-inline md-form mr-auto mb-4 float-md-right">
-  <input name="buscarpor" class="form-control mr-sm-2" type="text" placeholder="Buscar por cliente" aria-label="Search">
-  <button class="btn aqua-gradient btn-rounded btn-sm my-0" type="submit">Buscar</button>
-</form> -->
+</nav>
 
 
 
-                    <!--Table-->
-                    <!--<table class="table-striped w-auto mx-auto">-->
                         <table class="table table-hover">
-                            <!--Table head-->
                             <thead>
                                 <tr>
-                                    <!--<th>
-                                        <input class="form-check-input" type="checkbox" id="checkbox">
-                                        <label class="form-check-label" for="checkbox" class="mr-2 label-table"></label>
-                                    </th>-->
                                     <th class="th-lg text-center">
                                         <a>CLIENTE
-                                            <!--<i class="fas fa-sort ml-1"></i>-->
                                         </a>
                                     </th>
                                     <th class="th-lg text-center">
                                         <a>QUINCENA
-                                            <!--<i class="fas fa-sort ml-1"></i>-->
                                         </a>
                                     </th>
                                     <th class="th-lg text-center">
                                         <a>Num. FACTURA
-                                            <!--<i class="fas fa-sort ml-1"></i>-->
                                         </a>
                                     </th>
                                     <th class="th-lg text-center" hidden="true">
                                         <a>IMPORTE
-                                            <!--<i class="fas fa-sort ml-1"></i>-->
                                         </a>
                                     </th>
                                     <th class="th-lg text-center">
                                         <a>MONTO COBRADO
-                                            <!--<i class="fas fa-sort ml-1"></i>-->
                                         </a>
                                     </th>
                                     <th class="th-lg text-center" hidden="true">
                                         <a>GASTOS BANCARIOS
-                                            <!--<i class="fas fa-sort ml-1"></i>-->
                                         </a>
                                     </th>
                                     <th class="th-lg text-center" hidden="true">
                                         <a>DISPONIBLE
-                                            <!--<i class="fas fa-sort ml-1"></i>-->
                                         </a>
                                     </th>
                                     <th class="th-lg text-center">
                                         <a>TOTAL PAGO
-                                            <!--<i class="fas fa-sort ml-1"></i>-->
                                         </a>
                                     </th>
                                     <th class="th-lg text-center" hidden="true">
                                         <a>NETO QCNA
-                                            <!--<i class="fas fa-sort ml-1"></i>-->
                                         </a>
                                     </th>
                                     <th class="th-lg text-center">
                                         <a>COSTO TN
-                                            <!--<i class="fas fa-sort ml-1"></i>-->
                                         </a>
                                     </th>
 
                                 </tr>
                             </thead>
-                            <!--Table head-->
 
-                            <!--Table body-->
                             <tbody>
 
                                 @foreach ($controles as $control)
                                 <tr>
 
 
-                                    <!--<th scope="row">
-                                        <input class="form-check-input" type="checkbox" id="checkbox1">
-                                        <label class="form-check-label" for="checkbox1" class="label-table"></label>
-                                    </th>-->
                                     <?php $disponible = $control->importe - ($control->retencion + $control->gasto_bancario); ?>
                                     <?php $totalPago = $control->pago_personal + $control->pago_transporte; ?>
 
@@ -162,22 +198,9 @@
                                             <button type="submit" onclick="return confirm('¿Desea eliminar el control quincenal?')" id="borrar" class="btn peach-gradient mb-1 btn-sm m-0 text-center"> BORRAR
                                             </button>
 
-                                            <!--<button class="btn btn-danger" type="submit" id="borrar">Borrar</button>-->
                                         </form>
-                                        <!-- <form method="POST" action="">
-
-                                        <button class="btn btn-danger btn-rounded mb-4" type="submit" id="borrar">Borrar</button>
-                                    </form> -->
                                     </td>
-                                    <!--BOTON MODIFICAR NO FUNCIONA LA VISTA MODIFPRODUCTO, SI TOMA EL ID DEL PREODUCTO------
-                                <td>
-                                     <a id="modificar" class="btn btn-primary btn-rounded mb-4" href="/modif_control/{{$control->id}}" role="button" >Modificar </a>-->
-
-                                    <!-- <form method="POST" action="">
-                                        <button class="btn btn-primary btn-rounded mb-4" type="submit" id="borrar">Modifica</button>
-                                    </form>
-                                </td> -->
-
+                               
                                     <td>
                                         <form method="PUT" action="/modal_control/{{ $control->id }}">
                                             @csrf
@@ -195,17 +218,16 @@
 
 
                             </tbody>
-                            <!--Table body-->
                         </table>
 
 
-                        <!--Table-->
                 </div>
 
             </div>
         </div>
     </div>
 </div>
+-->
 <!--Section: Content-->
 {{ $controles->appends($_GET)->links() }}
 
