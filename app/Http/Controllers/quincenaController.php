d3759d23694ec0397bdc329caf703a78b86ff003
Alfredo <alfredosarria@hotmail.com>
2020-02-15 17:44:34 -0300

    .

--- app/Http/Controllers/quincenaController.php
+++ app/Http/Controllers/quincenaController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Quincena;
 use App\Control;
+use App\Quincena;
 use Laracasts\Flash\Flash;
 
 class quincenaController extends Controller
