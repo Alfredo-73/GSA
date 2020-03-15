b5a9e82fcfbcb1d3aaff4b7bab0b0a421e3a8702
Alfredo Sarria <alfredosarria@hotmail.com>
2020-03-15 20:53:03 -0300

    algunas correcciones en inicio

--- resources/views/cosecha.blade.php
+++ resources/views/cosecha.blade.php
@@ -14,106 +14,106 @@
             <div class="px-4">
                 <div class="table-wrapper">
                     <h1 class="text-center mb-5" style="font-family:Verdana, Geneva, Tahoma, sans-serif">PARTE DIARIO DE COSECHA</h1>
-                     @can('agregar_cosecha')
+                    @can('agregar_cosecha')
                     @endcan
                 </div>
                 <a id="agregar" class="btn primary-color-dark mb-5 rounded agregar" href="{{ url('/nueva_cosecha') }}" role="button" style="color:white"><i class="fas fa-2x fa-plus mr-2" style="color:white"></i>NUEVO </a>
 
-            <div class="container-fluid">
-                <nav class="navbar  navbar-dark indigo rounded mb-2">
-                    <span style="font-size:15px; font-family:Verdana, Geneva, Tahoma, sans-serif" class="text-white ml-5">INGRESE RANGO DE FECHA Y/O CAPATAZ:</span>
-                    <form class="form-inline">
-                        <input class="md-form mr-2 text-center rounded" type="date" placeholder="Desde" aria-label="Search" name="fechadesde" role="button">
-                        <input class="md-form mr-2 text-center rounded" type="date" placeholder="Hasta" aria-label="Search" name="fechahasta" role="button">
-                        <select class="selectpicker show-menu-arrow" name="buscacapataz" value="">
-                            <option>Capataz</option>
-                            @foreach($capataz as $capat)
-                            <option value="{{$capat->id}}">{{$capat->nombre}}</option>
-                            @endforeach
-                        </select>
-                        <button class="btn blue-gradient btn-rounded btn-sm my-0"><i class="fas fa-search fa-2x mr-2" style="color:white" name="buscar"></i>Buscar</button>
+                <div class="container-fluid">
+                    <nav class="navbar  navbar-dark indigo rounded mb-2">
+                        <span style="font-size:15px; font-family:Verdana, Geneva, Tahoma, sans-serif" class="text-white ml-5">INGRESE RANGO DE FECHA Y/O CAPATAZ:</span>
+                        <form class="form-inline">
+                            <input class="md-form mr-2 text-center rounded" type="date" placeholder="Desde" aria-label="Search" name="fechadesde" role="button">
+                            <input class="md-form mr-2 text-center rounded" type="date" placeholder="Hasta" aria-label="Search" name="fechahasta" role="button">
+                            <select class="selectpicker show-menu-arrow" name="buscacapataz" value="">
+                                <option>Capataz</option>
+                                @foreach($capataz as $capat)
+                                <option value="{{$capat->id}}">{{$capat->nombre}}</option>
+                                @endforeach
+                            </select>
+                            <button class="btn blue-gradient btn-rounded btn-sm my-0"><i class="fas fa-search fa-2x mr-2" style="color:white" name="buscar"></i>Buscar</button>
 
-                        <a href="{{ url('/cosecha') }}" title="Refrescar" name="Refrescar" style="color:white; font-family:Verdana, Geneva, Tahoma, sans-serif"><i class="fas fa-sync-alt ml-1" style="color:white"></i>Refrescar</a>
-                        @if(($varfechadesde || $varfechahasta) || ($varbuscacapataz) && ($varbuscacapataz !='Capataz'))<a role="button" class="btn btn-deep-orange" href="verreportecosechaPDF/{{$varfechadesde}}/{{$varfechahasta}}/{{$varbuscacapataz}}"><i class="fas fa-print mr-2" style="color:white"></i>Imprimir Busqueda</a> @endif
-                        @if((empty($varfechadesde) && empty($varfechahasta) || ($varbuscacapataz=='Capataz')) && (($varbuscacapataz =='Capataz') && ($varfechadesde ==null) && ($varfechahasta==null)))<a role="button" class="btn btn-deep-orange" href="verreportecosechaPDF"><i class="fas fa-print mr-2" style="color:white"></i>Imprimir Reporte</a> @endif
-                    </form>
-                </nav>
-            </div>
+                            <a href="{{ url('/cosecha') }}" title="Refrescar" name="Refrescar" style="color:white; font-family:Verdana, Geneva, Tahoma, sans-serif"><i class="fas fa-sync-alt ml-1" style="color:white"></i>Refrescar</a>
+                            @if(($varfechadesde || $varfechahasta) || ($varbuscacapataz) && ($varbuscacapataz !='Capataz'))<a role="button" class="btn btn-deep-orange" href="verreportecosechaPDF/{{$varfechadesde}}/{{$varfechahasta}}/{{$varbuscacapataz}}"><i class="fas fa-print mr-2" style="color:white"></i>Imprimir Busqueda</a> @endif
+                            @if((empty($varfechadesde) && empty($varfechahasta) || ($varbuscacapataz=='Capataz')) && (($varbuscacapataz =='Capataz') && ($varfechadesde ==null) && ($varfechahasta==null)))<a role="button" class="btn btn-deep-orange" href="verreportecosechaPDF"><i class="fas fa-print mr-2" style="color:white"></i>Imprimir Reporte</a> @endif
+                        </form>
+                    </nav>
+                </div>
 
-            {{ $cosechas->appends($_GET)->links() }}
-            <!--Table-->
-            <!--<table class="table-striped w-auto">-->
-            <table class="table table-bordered table-hover">
+                {{ $cosechas->appends($_GET)->links() }}
+                <!--Table-->
+                <!--<table class="table-striped w-auto">-->
+                <table class="table table-bordered table-hover">
 
-                <!--Table head-->
-                <thead class="thead-dark">
-                    <tr height="60px" style="background-color:black; color:white">
-                        <!--<th>
+                    <!--Table head-->
+                    <thead class="thead-dark">
+                        <tr height="60px" style="background-color:black; color:white">
+                            <!--<th>
                                         <input class="form-check-input" type="checkbox" id="checkbox">
                                         <label class="form-check-label" for="checkbox" class="mr-2 label-table"></label>
                                     </th>-->
-                        <th class="text-center">
-                            <a>FECHA
-                                <!--<i class="fas fa-sort ml-1"></i>-->
-                            </a>
-                        </th>
-                        <th class="text-center" hidden="true">
-                            <a>CLIENTE
-                                <!--<i class="fas fa-sort ml-1"></i>-->
-                            </a>
-                        </th>
-                        <th class="text-center">
-                            <a>CAPATAZ
-                                <!--<i class="fas fa-sort ml-1"></i>-->
-                            </a>
-                        </th>
-                        <th class="text-center">
-                            <a>JORNALES
-                                <!--<i class="fas fa-sort ml-1"></i>-->
-                            </a>
-                        </th>
-                        <th class="text-center">
-                            <a>COSECHEROS
-                                <!--<i class="fas fa-sort ml-1"></i>-->
-                            </a>
-                        </th>
-                        <th class="text-center">
-                            <a>BINES
-                                <!--<i class="fas fa-sort ml-1"></i>-->
-                            </a>
-                        </th>
-                        <th class="text-center">
-                            <a>MALETAS
-                                <!--<i class="fas fa-sort ml-1"></i>-->
-                            </a>
-                        </th>
-                        <th class="text-center">
-                            <a>TONELADAS
-                                <!--<i class="fas fa-sort ml-1"></i>-->
-                            </a>
-                        </th>
-                        <th class="text-center">
-                            <a>PROM. KG/BIN
-                                <!--<i class="fas fa-sort ml-1"></i>-->
-                            </a>
-                        </th>
+                            <th class="text-center">
+                                <a>FECHA
+                                    <!--<i class="fas fa-sort ml-1"></i>-->
+                                </a>
+                            </th>
+                            <th class="text-center" hidden="true">
+                                <a>CLIENTE
+                                    <!--<i class="fas fa-sort ml-1"></i>-->
+                                </a>
+                            </th>
+                            <th class="text-center">
+                                <a>CAPATAZ
+                                    <!--<i class="fas fa-sort ml-1"></i>-->
+                                </a>
+                            </th>
+                            <th class="text-center">
+                                <a>JORNALES
+                                    <!--<i class="fas fa-sort ml-1"></i>-->
+                                </a>
+                            </th>
+                            <th class="text-center">
+                                <a>COSECHEROS
+                                    <!--<i class="fas fa-sort ml-1"></i>-->
+                                </a>
+                            </th>
+                            <th class="text-center">
+                                <a>BINES
+                                    <!--<i class="fas fa-sort ml-1"></i>-->
+                                </a>
+                            </th>
+                            <th class="text-center">
+                                <a>MALETAS
+                                    <!--<i class="fas fa-sort ml-1"></i>-->
+                                </a>
+                            </th>
+                            <th class="text-center">
+                                <a>TONELADAS
+                                    <!--<i class="fas fa-sort ml-1"></i>-->
+                                </a>
+                            </th>
+                            <th class="text-center">
+                                <a>PROM. KG/BIN
+                                    <!--<i class="fas fa-sort ml-1"></i>-->
+                                </a>
+                            </th>
 
-                        <th class="text-center" hidden="true">
-                            <a>SUPERVISOR
-                                <!--<i class="fas fa-sort ml-1"></i>-->
-                            </a>
-                        </th>
-                        <th COLSPAN=2 class="text-center">ACCIONES DISPONIBLES</th>
-                    </tr>
+                            <th class="text-center" hidden="true">
+                                <a>SUPERVISOR
+                                    <!--<i class="fas fa-sort ml-1"></i>-->
+                                </a>
+                            </th>
+                            <th COLSPAN=2 class="text-center">ACCIONES DISPONIBLES</th>
+                        </tr>
 
-                </thead>
-                <!--Table head-->
+                    </thead>
+                    <!--Table head-->
 
-                <!--Table body-->
-                <tbody>
-                    @foreach ($cosechas as $cosecha)
-                    <tr>
-                        <!--<th scope="row">
+                    <!--Table body-->
+                    <tbody>
+                        @foreach ($cosechas as $cosecha)
+                        <tr>
+                            <!--<th scope="row">
                                         <input class="form-check-input" type="checkbox" id="checkbox1">
                                         <label class="form-check-label" for="checkbox1" class="label-table"></label>
                                     </th>-->
@@ -127,7 +127,7 @@
                             <td class="text-center"> {{$cosecha->toneladas}}</td>
                             <td class="text-center"> {{$cosecha->prom_kg_bin}}</td>
                             <td class="text-center" hidden="true"> {{$cosecha->supervisor}}</td>
-                           @can('borrar_cosecha')
+                            @can('borrar_cosecha')
                             <td class="text-center">
                                 <form method="POST" action=" {{ url('/borrar_cosecha/'.$cosecha->id) }}">
                                     {{ csrf_field() }}
@@ -140,11 +140,11 @@
                                 </form>
                             </td>
                             @endcan
-                            <td class="text-center">
+                            <td>
                                 <form method="PUT" action="/modalcosecha/{{$cosecha->id}}">
                                     @csrf
                                     {{method_field('PUT')}}
-                                    <a type="button" class="btn blue-gradient btn-sm" href="/modalcosecha/{{ $cosecha->id }}" data-toggle="modal" data-target="#modalcosecha{{ $cosecha->id }}" form method="POST" action="/modalcosecha/{{$cosecha->id}}" role="button"><i class="fas fa-eye mr-1" style="color:white"></i>VER</a>
+                                    <a type="button" class="btn blue-gradient btn-sm" href="/modalcosecha/{{ $cosecha->id }}" data-toggle="modal" data-target="#modalcosecha{{ $cosecha->id }}" form method="POST" action="/modalcosecha/{{$cosecha->id}}" role="button" style="text-align:justify"><i class="fas fa-eye mr-1" style="color:white"></i>VER</a>
                                     @csrf
                                     {{method_field('PUT')}}
                                     @include('modalcosecha')
