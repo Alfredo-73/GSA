3132b7f7be2fba24a0ba1351e73f28c910932b20
Alfredo Sarria <alfredosarria@hotmail.com>
2020-04-03 19:50:21 -0300

    arreglo inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class=" container-fluid">
 
-    <div class="view overlay col-10 mt-5">
+    <div class="view overlay col-10" style="margin-top:5rem">
         <img src="{{ asset('img/gsagricolas.jpg')}}" class="img-fluid mx-auto" alt="Sample image with waves effect.">
         <a>
             <!--<div class="mask waves-effect rgba-white-slight"></div>-->
